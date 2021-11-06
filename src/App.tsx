2a58ba18aa152a98f747522a9e321eb75e90aa46
Elgatou <elgatouv@gmail.com>
2021-11-06 15:56:37 +0700

    add features folder, move all components and logic in it

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
-import { TodoItemsList } from "./TodoItems";
-import { TodoItemsContextProvider } from "./TodoItemsContext";
-import TodoItemForm from "./TodoItemForm";
+import { TodoItemsList } from "./features/todoItems/TodoItems";
+import { TodoItemsContextProvider } from "./features/todoItems/TodoItemsContext";
+import TodoItemForm from "./features/todoItems/TodoItemForm";
 
 const theme = createMuiTheme({
   palette: {
